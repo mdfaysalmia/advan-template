832bf5f0c7245cadb0bcc084858473321737eb21
mdfaysalmia <faysalmia7292@gmail.com>
2024-06-09 22:28:22 +0600

    resposive style added

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// Slide JS Active
+var swiper = new Swiper(".testimonial-slider", {
+  slidesPerView: 1,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
+
+// Animation JS 
+new WOW().init();
